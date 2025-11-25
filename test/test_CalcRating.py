ebb668f08e36c59e0f9f121bab7b3a178aaf370f
Anton <DarrenHD@yandex.ru>
2025-11-26 00:07:31 +0300

    Fix PEP8 formatting

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -40,4 +40,5 @@ class TestCalcRating:
         rating = CalcRating(input_data[0]).calc()
         for student in rating:
             rating_score = rating[student]
-            assert pytest.approx(rating_score, abs=0.001) == input_data[1][student]
+            assert pytest.approx(rating_score, 
+                                 abs=0.001) == input_data[1][student]
